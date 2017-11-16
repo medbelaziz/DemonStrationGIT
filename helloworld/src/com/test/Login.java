fcc88f2c19b00ff98a0d1c57d0284fb7d9872fbd
Belaziz Mohammed <mbelaziz@sqli.com>
2017-11-16 22:17:34 +0000

    2

--- helloworld/src/com/test/Login.java
+++ helloworld/src/com/test/Login.java
@@ -17,11 +17,9 @@ public class Login extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
 	}
 
 }
